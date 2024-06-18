324f5ede60f1f7dfd664fefc1697c59079dd3250
Orlando <goro.14082@gmail.com>
2024-06-18 09:52:27 -0400

    task 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers from 0 to 9.
+ * main - Prints numbers between 0 to 9.
  *
  * Return: Always 0 (Success)
  */
@@ -9,9 +9,9 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		printf("%d", i);
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
