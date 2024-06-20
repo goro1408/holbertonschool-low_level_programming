05f52765bdf0de8cd7209a7db4ce92f995c7454e
Orlando <goro.14082@gmail.com>
2024-06-20 10:10:40 -0400

    main

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,25 +1,19 @@
-int _putchar(char c);
+int _isupper(int c);
 
-void print_alphabet(void);
+int _isdigit(int c);
 
-void print_alphabet_x10(void);
+int mul(int a, int b);
 
-int _islower(int c);
+void print_numbers(void);
 
-int _isalpha(int c);
+void print_most_numbers(void);
 
-int print_sign(int n);
+void more_numbers(void);
 
-int _abs(int);
+void print_line(int n);
 
-int print_last_digit(int);
+void print_diagonal(int n);
 
-void jack_bauer(void);
+void print_square(int size);
 
-void times_table(void);
-
-int add(int, int);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
+void print_triangle(int size);
