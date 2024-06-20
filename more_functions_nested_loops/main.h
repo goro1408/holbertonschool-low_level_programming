9b23970fe5782d6def2b759074237df3090680be
Orlando <goro.14082@gmail.com>
2024-06-20 11:02:42 -0400

    main

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+int _putchar(char c);
+
 int _isupper(int c);
 
 int _isdigit(int c);
@@ -17,3 +19,5 @@ void print_diagonal(int n);
 void print_square(int size);
 
 void print_triangle(int size);
+
+void print_number(int n)
