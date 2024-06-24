645ddad5027ee9d3d4e3807fdb8de409747df731
Orlando <goro.14082@gmail.com>
2024-06-24 09:36:21 -0400

    Task 1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 /**
- * swap_int - switches value of the first input with the value of the second input.
- * @a: First value to swap
- * @b Second value to swap
+ * swap_int - switches value of first input with the value of the second input.
+ * @a: First value to swap.
+ * @b: Second value to swap.
  */
 void swap_int(int *a, int *b)
 {
-	int temp:
+	int temp;
 
 	temp = *a;
 	*a = *b;
