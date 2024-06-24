42b351c6176181759c1bead710ca83bd6108438b
Orlando <goro.14082@gmail.com>
2024-06-24 09:43:57 -0400

    Task 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,9 @@
-nt _strlen(char *s)
+/**
+ * _strlen - calculates the length of a string.
+ * @s: pointer to the character array.
+ * Return: length of the string
+ */
+int _strlen(char *s)
 {
 	int 1;
 
