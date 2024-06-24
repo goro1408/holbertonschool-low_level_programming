e4ae12cb5c593b68bd77c8e9244b1e817b62fd0f
Orlando <goro.14082@gmail.com>
2024-06-24 10:00:08 -0400

    main

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,12 +1,25 @@
-int void reset_to_98(int *n);
-int void swap_int(int *a, int *b);
-int int _strlen(char *s);
-int void _puts(char *str);
-int void print_rev(char *s);
-int void rev_string(char *s);
-int void puts2(char *str);
-int void puts_half(char *str);
-int void print_array(int *a, int n);
-int char *_strcpy(char *dest, char *src);
-int int _atoi(char *s);
-int _putchar(char c)
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+
+#endif
