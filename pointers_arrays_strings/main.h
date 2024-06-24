ce3847fb0d3ab2e23b0d4b8f54d86958e811f4dd
Orlando <goro.14082@gmail.com>
2024-06-24 09:28:58 -0400

    README

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+int void reset_to_98(int *n);
+int void swap_int(int *a, int *b);
+int int _strlen(char *s);
+int void _puts(char *str);
+int void print_rev(char *s);
+int void rev_string(char *s);
+int void puts2(char *str);
+int void puts_half(char *str);
+int void print_array(int *a, int n);
+int char *_strcpy(char *dest, char *src);
+int int _atoi(char *s);
