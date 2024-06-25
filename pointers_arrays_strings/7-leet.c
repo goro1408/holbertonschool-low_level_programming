8e1ec650591530d8c2a8130b626b8648a03b71b2
Orlando <goro.14082@gmail.com>
2024-06-25 10:34:51 -0400

    Task 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,22 +9,21 @@ char *leet(char *s)
 {
 	int count = 0, i;
 	int low_letters[] = {97, 101, 111, 116, 108};
-	int upp_letters[] = {65, 59, 79, 84, 76};
+	int upp_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
 
 	while (*(s + count) != '\0')
 	{
-		for (i = 0; 1 < 5; i++)
+		for (i = 0; i < 5; i++)
 		{
-			if (*(s + count) == low_letters[i] || *(s + count) ==
-					{
-					*(s + count) = numbers[i];
-					break;
-					}
+			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+			{
+				*(s + count) = numbers[i];
+				break;
+			}
 		}
-		}
-	return (s);
+		count++;
 	}
 
-
-
+	return (s);
+}
