e7492176e8fffbcc6e026ec2d3de4998c7bd1574
Orlando <goro.14082@gmail.com>
2024-06-26 13:30:57 -0400

    Task 9

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -13,6 +13,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+void print_number(int n)
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -21,5 +22,6 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *rot13(char *);
 
 #endif
