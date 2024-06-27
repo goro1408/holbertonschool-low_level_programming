71db643b7f37aed4467eaa8b17a6d513eec9c220
Orlando <goro.14082@gmail.com>
2024-06-27 09:49:13 -0400

    Task 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _strchr - location a character in a string
+ * _strchr - locates a character in a string,
  * @s: string.
  * @c: character.
- * Return: the pointer to the first ocurrence of the character c.
+ * Return: the pointer to the first occurrence of the character c.
  */
-char *_strchr(char *s, char c);
+char *_strchr(char *s, char c)
 {
-	unsigned int 1 = 0;
+	unsigned int i = 0;
 
 	for (; *(s + i) != '\0'; i++)
 		if (*(s + i) == c)
